144b1bc891f098363e81c4aa8d9e8175f21abbd6
sagar691996 <w0857561@myscc.ca>
2025-03-21 10:51:35 -0400

    Added commenting and necessary changes in the layout

--- app/src/main/java/com/example/assignment_1_sagarparmar_0857562/Adapter.kt
+++ app/src/main/java/com/example/assignment_1_sagarparmar_0857562/Adapter.kt
@@ -7,21 +7,25 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
+//this class is for the individual expenses
 class ExpenseAdapter(private val expenseList: ArrayList<MainActivity.Expense>) :
     RecyclerView.Adapter<ExpenseAdapter.ExpenseViewHolder>() {
 
+        // ExpenseViewHolder
     class ExpenseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val expensesName: TextView = itemView.findViewById(R.id.expensesName)
         val expenseAmount: TextView = itemView.findViewById(R.id.expenseAmount)
         val deleteButton: Button = itemView.findViewById(R.id.delete_button)
     }
 
+    //oncreateViewHolder is fetch the data from the parent class to single tabview recycleView ad return the proper details to perform task
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExpenseViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.single_tabview_recycleview, parent, false)
         return ExpenseViewHolder(view)
     }
 
+    //OnBindingViewHolder is fethcing the data from the holder and added to the single tabview holder
     override fun onBindViewHolder(holder: ExpenseViewHolder, position: Int) {
         val expense = expenseList[position]
         holder.expensesName.text = expense.name
--- app/src/main/java/com/example/assignment_1_sagarparmar_0857562/MainActivity.kt
+++ app/src/main/java/com/example/assignment_1_sagarparmar_0857562/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.assignment_1_sagarparmar_0857562
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -12,10 +13,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var expenseList: ArrayList<Expense>
     private lateinit var adapter: ExpenseAdapter
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Fetch the data from xml file: activity main
         val expenseInput = findViewById<EditText>(R.id.editTextName)
         val expenseInputAmount = findViewById<EditText>(R.id.editTextName1)
         val submitButton = findViewById<Button>(R.id.button)
@@ -34,7 +37,11 @@ class MainActivity : AppCompatActivity() {
 
             if (name.isNotEmpty() && amount.isNotEmpty()) {
                 expenseList.add(Expense(name, amount))
-                adapter.notifyDataSetChanged()
+                try {
+                    adapter.notifyDataSetChanged()
+                } catch (e: Exception) {
+                    TODO("Not yet implemented")
+                }
                 expenseInput.text.clear()
                 expenseInputAmount.text.clear()
             }
