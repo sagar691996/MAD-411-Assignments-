156304327da07a65898f2acfa38bae484469da60
sagar691996 <w0857561@myscc.ca>
2025-03-20 22:17:23 -0400

    Added the editText for enter the expenses and button for apply expeses to add in a list

--- app/src/main/java/com/example/assignment_1_sagarparmar_0857562/MainActivity.kt
+++ app/src/main/java/com/example/assignment_1_sagarparmar_0857562/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.assignment_1_sagarparmar_0857562
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +18,6 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    fun ShowName(view: View) {}
 }
